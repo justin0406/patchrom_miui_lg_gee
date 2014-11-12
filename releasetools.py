60e988a55a10409b4b2af289eb84bd523efcd7d7
JustinXu <411488070@qq.com>
2014-11-12 16:43:42 +0800

    Create releasetools.py

---
+++ releasetools.py
@@ -0,0 +1,29 @@
+import common
+
+import edify_generator
+
+ 
+
+def RemoveDeviceAssert(info):
+
+  edify = info.script
+
+  for i in xrange(len(edify.script)):
+
+    if "ro.product" in edify.script[i]:
+
+      edify.script[i] = ''
+
+      return
+
+ 
+
+def FullOTA_InstallEnd(info):
+
+    RemoveDeviceAssert(info)
+
+ 
+
+def IncrementalOTA_InstallEnd(info):
+
+    RemoveDeviceAssert(info)

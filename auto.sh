4863e611c1e8355b04c98f8324e8b4174db8d21c
JustinXu <411488070@qq.com>
2015-06-28 01:16:10 +0800

    Update and rename ota.sh to auto.sh

--- auto.sh
+++ auto.sh
@@ -5,9 +5,10 @@
 #BUILD_NUMBER=5.5.13
 #LAST_TARGET=last_target_files_5.5.8.zip(last make fullota with target files zip)
 BUILD_NUMBER=$1
-LAST_TARGET=$2
+LAST_BUILD_NUMBER=$2
+LAST_TARGET=$3
 if [ $# != 2 ];then
-    echo "Usage: ./auto_make.sh   BUILD_NUMBER   LAST_TARGET"
+    echo "Usage: ./auto_make.sh   BUILD_NUMBER  LAST_BUILD_NUMBER  LAST_TARGET"
     exit
 fi
 echo "******************"
@@ -36,7 +37,7 @@ echo "------------------"
 echo "    make ota?     "
 echo "------------------"
 read
-../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $LAST_TARGET out/target_files.zip OTA-$LAST_TARGET-$BUILD_NUMBER.zip
+../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $LAST_TARGET out/target_files.zip OTA-$LAST_BUILD_NUMBER-to-$BUILD_NUMBER.zip
 echo "******************"
 echo "*Copy  Target.zip*"
 echo "******************"
